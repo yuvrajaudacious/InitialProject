3d838164f971c4ca295556b504de83dc517456fb
yuvrajaudacious <audaciousyuvraj227@gmail.com>
2022-12-21 21:43:26 +0530

    change signup page

--- src/Component/Adduser/Add.jsx
+++ src/Component/Adduser/Add.jsx
@@ -15,6 +15,15 @@ function Add() {
   };
   return (
     <>
+    <section class="h-100 h-custom" style="background-color: #8fc4b7;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-8 col-xl-6">
+        <div class="card rounded-3">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img3.webp"
+            class="w-100" style="border-top-left-radius: .3rem; border-top-right-radius: .3rem;"
+            alt="Sample photo">
+          <div class="card-body p-4 p-md-5"></div>
           <div className={`${styles.bg}`}>
 
       <div>
@@ -81,6 +90,14 @@ function Add() {
         </div>
       </div>
       </div>
+      </img>
+      
+      </div>
+      </div>
+      </div></div>
+      </section>
+
+      
     </>
   );
 }
--- src/Pages/Signup/Index.jsx
+++ src/Pages/Signup/Index.jsx
@@ -32,12 +32,15 @@ const Index = () => {
           <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-lg-12 col-xl-11">
               <div class="card text-black">
-                <div class="card-body p-md-5">
+                <div class={`card-body p-md- ${styles.b}`}>
                   <div class="row justify-content-center">
                     <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                        Sign up
-                      </p>
+                      <img
+                      src="https://www.freeiconspng.com/thumbs/sign-up-button-png/sign-up-button-png-2.png"
+                      class="img-fluid"
+                      alt="Sample image"
+                    />                      </p>
                       <Formik
                         initialValues={{
                           name: "",
@@ -56,7 +59,7 @@ const Index = () => {
                           <Form class="mx-1 mx-md-4">
                             <div class="d-flex flex-row align-items-center mb-4">
                               <div class="form-outline flex-fill mb-0">
-                                <label class="form-label">Name</label>
+                                <label class="form-label"><b>Name</b></label>
                                 <Field class="form-control" name="name" />
                                 {errors.name && touched.name ? (
                                   <div style={{ color: "red" }}>
@@ -68,7 +71,7 @@ const Index = () => {
 
                             <div class="d-flex flex-row align-items-center mb-4">
                               <div class="form-outline flex-fill mb-0">
-                                <label class="form-label">Phone Number</label>
+                                <label class="form-label"><b>Phone Number</b></label>
                                 <Field
                                   class="form-control"
                                   name="phoneNumber"
@@ -84,7 +87,7 @@ const Index = () => {
 
                             <div class="d-flex flex-row align-items-center mb-4">
                               <div class="form-outline flex-fill mb-0">
-                                <label class="form-label">Email</label>
+                                <label class="form-label"><b>Email</b></label>
 
                                 <Field
                                   class="form-control"
@@ -101,7 +104,7 @@ const Index = () => {
 
                             <div class="d-flex flex-row align-items-center mb-4">
                               <div class="form-outline flex-fill mb-0">
-                                <label class="form-label">Password</label>
+                                <label class="form-label"><b>Password</b></label>
                                 <Field
                                   class="form-control"
                                   type="password"
@@ -116,7 +119,7 @@ const Index = () => {
                             </div>
                             <div class="d-flex flex-row align-items-center mb-4">
                               <div class="form-outline flex-fill mb-0">
-                                <label class="form-label">Address</label>
+                                <label class="form-label"><b>Address</b></label>
                                 <Field
                                   class="form-control"
                                   type="address"
@@ -132,7 +135,7 @@ const Index = () => {
                             <div class="d-flex flex-row align-items-center mb-4">
                               <div class="form-outline flex-fill mb-0">
                                 <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-                                  <label class="mb-0 me-4">Gender: </label>
+                                  <label class="mb-0 me-4"><b>Gender: </b></label>
 
                                   <div class="form-check form-check-inline mb-0 me-4">
                                     <input
@@ -193,20 +196,20 @@ const Index = () => {
                                 Sign in
                               </button>
                             </div>
-                            <Link className=" mx-1 text-info" to="/">
+                            <Link  className={`mx-1 text-info ${styles.al}`}  to="/">
                               <b>Already Login </b>
                             </Link>
                           </Form>
                         )}
                       </Formik>
                     </div>
-                    <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        class="img-fluid"
-                        alt="Sample image"
-                      />
-                    </div>
+                  <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp"
+                      class="img-fluid"
+                      alt="Sample image"
+                    />
+                  </div>
                   </div>
                 </div>
               </div>
